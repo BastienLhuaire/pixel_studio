32ef054783589a721265e296723e9510c040e4b2
Bastien Lhuaire <bastien.lhuaire@outlook.fr>
2016-10-11 12:54:44 +0200

    fin v.01 de Pixel Studio

--- app/pencil.class.js
+++ app/pencil.class.js
@@ -7,24 +7,23 @@ class Pencil extends Tool{
 	}
 
 	on_mousedown(mouse_event){
-		let	ps 		 = pixel_studio,
-			position = this.canvas.screen_to_canvas(mouse_event),
-			color = ps.palette_color.get_selected();
-
-		this.canvas.draw(position.x,position.y, color);
+		this.draw(mouse_event);
 		canvas.canDraw=true;
 	}
 
 	on_mousemove(mouse_event){
 		if (canvas.canDraw==true) {
-			let	ps 		 = pixel_studio,
-			position = this.canvas.screen_to_canvas(mouse_event),
-			color = ps.palette_color.get_selected();
-			this.canvas.draw(position.x,position.y, color);
+			this.draw(mouse_event);
 		};
 	}
 
 	on_mouseup(mouse_event){
 		canvas.canDraw=false;
+	}
+
+	draw( mouse_event){
+		let position = this.canvas.screen_to_canvas(mouse_event),
+			color 	 = pixel_studio.palette_color.get_selected();
+		this.canvas.draw(position.x,position.y, color);
 	}
 }
